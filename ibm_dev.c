96b7b7f1cb9bee67227c175877e51b8d30834974
RokasB25 <108929788+RokasB25@users.noreply.github.com>
2022-08-26 09:35:32 +0300

    Add files via upload

--- ibm_dev.c
+++ ibm_dev.c
@@ -112,12 +112,13 @@ void getopts(int argc, char** argv)
     }
 }
 
-/* 
+ /* 
  * Device command callback function
  * Device developers can customize this function based on their use case
  * to handle device commands sent by WIoTP.
  * Set this callback function using API setCommandHandler().
  */
+
 void  deviceCommandCallback (char* type, char* id, char* commandName, char *format, void* payload, size_t payloadSize)
 {
     fprintf(stdout, "Received device command:\n");
@@ -155,8 +156,16 @@ static const struct blobmsg_policy info_policy[__INFO_MAX] = {
 	[MEMORY_DATA] = { .name = "memory", .type = BLOBMSG_TYPE_TABLE },
 };
 
+struct memory_data{
+    char Total_memory[100];
+    char Free_memory[50];
+    char Shared_memory[50];
+    char Buffered_memory[50];
+};
+
 static void board_cb(struct ubus_request *req, int type, struct blob_attr *msg) {
-	struct blob_buf *buf = (struct blob_buf *)req->priv;
+	//struct blob_buf *buf = (struct blob_buf *)req->priv;
+    struct memory_data *temp_data = (struct memory_data *)req->priv;
 	struct blob_attr *tb[__INFO_MAX];
 	struct blob_attr *memory[__MEMORY_MAX];
 
@@ -169,56 +178,46 @@ static void board_cb(struct ubus_request *req, int type, struct blob_attr *msg) 
 	}
 
 	blobmsg_parse(memory_policy, __MEMORY_MAX, memory,
-			blobmsg_data(tb[MEMORY_DATA]), blobmsg_data_len(tb[MEMORY_DATA]));
-    sprintf(buf,"{\"d\" : {\"Total memory\" : %lld\"Free memory\" : %lld\"Shared memory\" : %lld\"Buffered memory\" : %lld}}",\
+	        blobmsg_data(tb[MEMORY_DATA]), blobmsg_data_len(tb[MEMORY_DATA]));
+
+    /* sprintf(buf,"{\"d\" : {\"Total memory\": %lld, \"Free memory\" : %lld, \"Shared memory\" : %lld, \"Buffered memory\" : %lld}}",\
     blobmsg_get_u64(memory[TOTAL_MEMORY]),blobmsg_get_u64(memory[FREE_MEMORY]),blobmsg_get_u64(memory[SHARED_MEMORY]),\
     blobmsg_get_u64(memory[BUFFERED_MEMORY]));
+    */
+    sprintf(temp_data->Total_memory,"%lld", blobmsg_get_u64(memory[TOTAL_MEMORY]));
+    printf("Printing Total memory %s\n", temp_data->Total_memory);
+
+    sprintf(temp_data->Free_memory,"%lld",blobmsg_get_u64(memory[FREE_MEMORY]));
+    printf("Printing Free memory %s\n", temp_data->Free_memory);
+
+    sprintf(temp_data->Shared_memory,"%lld",blobmsg_get_u64(memory[SHARED_MEMORY]));
+    printf("Printing shared memory %s\n", temp_data->Shared_memory);
+
+    sprintf(temp_data->Buffered_memory,"%lld",blobmsg_get_u64(memory[BUFFERED_MEMORY]));
+    printf("Printing buffered memory %s\n", temp_data->Buffered_memory);
+
+    //temp_data->Free_memory = blobmsg_get_u64(memory[FREE_MEMORY]);
+    //temp_data->Shared_memory = blobmsg_get_u64(memory[SHARED_MEMORY]);
+    //temp_data->Buffered_memory = blobmsg_get_u64(memory[SHARED_MEMORY]);
 }
+
+    
+
 
 /* Main program */
 int main(int argc, char *argv[])
 {
-    int rc = 0;
     int cycle = 0;
-    
-    
-    /*?FILE *fp= NULL;
-    pid_t process_id = 0;
-	pid_t sid = 0;
-	// Create child process
-	process_id = fork();
-	// Indication of fork() failure
-	if (process_id < 0) {
-		printf("fork failed!\n");
-		exit(1);
-	}
-	// PARENT PROCESS. Need to kill it.
-	if (process_id > 0) {
-		printf("process_id of child process %d \n", process_id);
-		exit(0);
-	}
-	//unmask the file mode
-	umask(0);
-	//set new session
-	sid = setsid();
-	if(sid < 0) {
-		exit(1);
-	}
-	// Change the current working directory to root.
-	chdir("/");
-	// Close stdin. stdout and stderr
-	close(STDIN_FILENO);
-	close(STDOUT_FILENO);
-	close(STDERR_FILENO);
-    */
+    //int rc =0;
 
-
+    IoTPConfig *config = NULL;
+    IoTPDevice *device = NULL;
     /* 
      * DEV_NOTES:
      * Specifiy variable for WIoT client object 
      */
-    IoTPConfig *config = NULL;
-    IoTPDevice *device = NULL;
+    
+    IOTPRC rc = IOTPRC_SUCCESS;
 
     /* check for args */
     if ( argc < 2 )
@@ -235,14 +234,16 @@ int main(int argc, char *argv[])
     rc = IoTPConfig_setLogHandler(IoTPLog_FileDescriptor, stdout);
     if ( rc != 0 ) {
         fprintf(stderr, "WARN: Failed to set IoTP Client log handler: rc=%d\n", rc);
-        exit(1);
+        goto close_error;
     }
+
+
 
     /* Create IoTPConfig object using configuration options defined in the configuration file. */
     rc = IoTPConfig_create(&config, configFilePath);
     if ( rc != 0 ) {
         fprintf(stderr, "ERROR: Failed to initialize configuration: rc=%d\n", rc);
-        exit(1);
+        goto close_error;
     }
 
     /* read additional config from environment */
@@ -250,25 +251,30 @@ int main(int argc, char *argv[])
         IoTPConfig_readEnvironment(config);
     } 
 
+    IoTPConfig_setProperty(config, "identity.orgId", "gut697");
+    IoTPConfig_setProperty(config, "identity.typeId", "roko_rutx");
+    IoTPConfig_setProperty(config, "identity.deviceId", "rutx10qwerty");
+    IoTPConfig_setProperty(config, "token.deviceId", "@WFY&!O9!9ghST+dOz");
+
     /* Create IoTPDevice object */
     rc = IoTPDevice_create(&device, config);
     if ( rc != 0 ) {
         fprintf(stderr, "ERROR: Failed to configure IoTP device: rc=%d\n", rc);
-        exit(1);
+        goto close_error;
     }
 
-    /* Set MQTT Trace handler */
+    /* Set MQTT Trace handler
     rc = IoTPDevice_setMQTTLogHandler(device, &MQTTTraceCallback);
     if ( rc != 0 ) {
         fprintf(stderr, "WARN: Failed to set MQTT Trace handler: rc=%d\n", rc);
-    }
+    } */
 
     /* Invoke connection API IoTPDevice_connect() to connect to WIoTP. */
     rc = IoTPDevice_connect(device);
     if ( rc != 0 ) {
         fprintf(stderr, "ERROR: Failed to connect to Watson IoT Platform: rc=%d\n", rc);
         fprintf(stderr, "ERROR: Returned error reason: %s\n", IOTPRC_toString(rc));
-        exit(1);
+        goto close_error;
     }
 
     /*
@@ -283,43 +289,37 @@ int main(int argc, char *argv[])
      * The arguments for this API are commandName, format, QoS
      * If you want to subscribe to all commands of any format, set commandName and format to "+"
      */
-    char *commandName = "+";
-    char *format = "+";
-    IoTPDevice_subscribeToCommands(device, commandName, format);
-
-
     /* Use IoTPDevice_sendEvent() API to send device events to Watson IoT Platform. */
     
 	struct ubus_context *ctx;
 	uint32_t id;
 
+    ctx = ubus_connect(NULL);
+	if (!ctx) {
+	fprintf(stderr, "Failed to connect to ubus\n");
+	return -1;
+	}
 	
 
     /* Sample event - this sample device will send this event once in every 10 seconds. */
-    //char *data = "{\"d\" : {\"SensorID\": \"Test\", \"Reading\": 7 }}";
+
+    struct memory_data *Data1;
+
     char *data[200];
+    //memset(data,' ',200);
+
     while(!interrupt)
     {
-
-
-        	ctx = ubus_connect(NULL);
-	if (!ctx) {
-		fprintf(stderr, "Failed to connect to ubus\n");
-		return -1;
-	}
-
         if (ubus_lookup_id(ctx, "system", &id) ||
-	    ubus_invoke(ctx, id, "info", NULL, board_cb, data, 3000)) {
+	    ubus_invoke(ctx, id, "info", NULL, board_cb, Data1, 3000)) {
 		fprintf(stderr, "cannot request memory info from procd\n");
 		rc=-1;
-	}
-
-        //printf("%s\n", buffer);
-
+	    }
+        printf("\nPrinting data in main %s\n", Data1->Total_memory);
         fprintf(stdout, "Send status event\n");
-        rc = IoTPDevice_sendEvent(device,"status", data, "json", QoS0, NULL);
-        printf("%s\n", data);
-        //free(buffer);
+
+
+        //rc = IoTPDevice_sendEvent(device,"status", data, "json", QoS0, NULL);
         fprintf(stdout, "RC from publishEvent(): %d\n", rc);
 
         if ( testCycle > 0 ) {
@@ -337,8 +337,14 @@ int main(int argc, char *argv[])
     rc = IoTPDevice_disconnect(device);
     if ( rc != IOTPRC_SUCCESS ) {
         fprintf(stderr, "ERROR: Failed to disconnect from  Watson IoT Platform: rc=%d\n", rc);
-        exit(1);
+        goto close_error;
     }
+
+close_error:
+        exit(1);
+
+close_noerror:
+        exit(0);
 
     /* Destroy client */
     IoTPDevice_destroy(device);
@@ -347,5 +353,4 @@ int main(int argc, char *argv[])
     IoTPConfig_clear(config);
     
     return 0;
-
 }
